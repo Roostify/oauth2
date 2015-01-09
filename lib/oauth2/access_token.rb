986641432e5bb209c4ea3847b9ed57db0063ad96
Jonathan Kirst <jskirst@gmail.com>
2015-01-08 16:47:27 -0800

    Being more direct.

--- lib/oauth2/access_token.rb
+++ lib/oauth2/access_token.rb
@@ -1,7 +1,7 @@
 module OAuth2
   class AccessToken
     attr_reader :client, :token, :expires_in, :expires_at, :params
-    attr_accessor :options, :refresh_token
+    attr_accessor :options, :refresh_token, :response
 
     class << self
       # Initializes an AccessToken from a Hash
@@ -48,7 +48,7 @@ module OAuth2
     def initialize(client, token, opts = {})
       @client = client
       @token = token.to_s
-      [:refresh_token, :expires_in, :expires_at].each do |arg|
+      [:refresh_token, :expires_in, :expires_at, :response].each do |arg|
         instance_variable_set("@#{arg}", opts.delete(arg) || opts.delete(arg.to_s))
       end
       @expires_in ||= opts.delete('expires')
@@ -58,7 +58,6 @@ module OAuth2
       @options = {:mode          => opts.delete(:mode) || :header,
                   :header_format => opts.delete(:header_format) || 'Bearer %s',
                   :param_name    => opts.delete(:param_name) || 'access_token'}
-      @get_token_response = opts.delete('get_token_response')
       @params = opts
     end
 
